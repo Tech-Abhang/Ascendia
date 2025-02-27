f7473b1bd3f3b381f7172c84d49132f23eb5c014
Abhang Pawar <abhangpawar03@gmail.com>
2025-02-27 19:16:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className="main">
+        <p>hi</p>
+        <Button className="ml-2">Log in</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
