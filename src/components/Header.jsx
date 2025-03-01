6a83bb4af402be513cab01e2b7871b93f67517fd
Abhang Pawar <abhangpawar03@gmail.com>
2025-03-01 16:22:01 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <nav className='py-4 flex justify-between items-center'>
+        <Link>
+          <img src='/logo.png' className='h-20'></img>
+        </Link>
+        <button varient="outline">Login</button>
+      </nav>
+    </div>
   )
 }
 
