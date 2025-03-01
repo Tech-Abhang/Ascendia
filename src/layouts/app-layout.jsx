a59264f9f1f227bb24691d9aa43237367f411ff8
Abhang Pawar <abhangpawar03@gmail.com>
2025-03-01 16:06:50 +0530

    error handled

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,13 +1,19 @@
+import Header from '@/components/Header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
-    <div>
-        <div className="grid-background"></div>
-        <Outlet/>
+    <div className='relative z-1'>
+      <div className="grid-background ">
+        <main className="h-screen">
+          <Header />
+          <Outlet />
+        </main>
+        <div className='p-10 text-center bg-gray-800 relative z-1'>Made By Abhang</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AppLayout
